6273903dba2adf7bcb00be848670e4fd1c9431ac
Darwin <darwin@selfdrvn.com>
2022-07-11 15:09:51 +0800

    Update AddAccount UI

--- src/component/AddAccount.jsx
+++ src/component/AddAccount.jsx
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './AddAccount.css';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+    field: {
+        height: 2,
+        fontSize: "10px"
+    }
+})
 
 const AddAccount = ({closeModal, brokerageData, setBrokerageData, regionData, groupData, isEdit, editId}) => {
     const [loading, setLoading] = useState(false);
     const [editData, setEditData] = useState([]);
     let lastArr = brokerageData[brokerageData.length - 1]
 
+    const classes = useStyles();
     const [obj1, setObj1] = useState({
             "id": lastArr.id + 1,
             "name": "",
@@ -73,10 +84,10 @@ const AddAccount = ({closeModal, brokerageData, setBrokerageData, regionData, gr
                 <div onClick={closeModal}>X</div>
             </div>
             <div>Form</div>
-            <form>
+            <form autoComplete="off">
                 <div>
                     <label>Brokerage Group</label>
-                    <select name="group" onChange={(e) => handleInputChange(e)} value={obj1.group}>
+                    {/* <select name="group" onChange={(e) => handleInputChange(e)} value={obj1.group}>
                     {
                         groupData && 
                             groupData.map((item, idx) => {
@@ -86,29 +97,72 @@ const AddAccount = ({closeModal, brokerageData, setBrokerageData, regionData, gr
                                 )}
                             )
                     }
-                    </select>
+                    </select> */}
+                    <TextField
+                        variant="outlined"
+                        select
+                        name="group"
+                        value={obj1.group}
+                        onChange={(e) => handleInputChange(e)}
+                        size="small"
+                        >
+                        {groupData && 
+                            groupData.map((group, idx) => {
+                                return (
+                                    <MenuItem key={idx} value={group.name}>{group.name}</MenuItem>
+
+                                )}
+                            )}
+                    </TextField>
                 </div>
                 <div>
                     <label>Brokerage Name</label>
-                    {/* <input type="text" name="name" onChange={(e) => handleInputChange(e)} value={isEdit && editData.map((item, i) => item.name)}/> */}
-                    <input type="text" name="name" onChange={(e) => handleInputChange(e)} value={obj1.name}/>
+                    {/* <input type="text" name="name" onChange={(e) => handleInputChange(e)} value={obj1.name}/> */}
+                    <div className="TextField-without-border-radius">
+                        <TextField 
+                            variant="outlined"
+                            // InputProps={{ classes: { input: classes.field } }}
+                            placeholder="Enter brokerage name"
+                            name="name" 
+                            onChange={(e) => handleInputChange(e)} 
+                            value={obj1.name}
+                            size="small"
+                            sx={{ borderRadius: 0 }}
+                        />
+                    </div>
                 </div>
                 <div>
                     <label>Region</label>
-                    <select name="region" onChange={(e) => handleInputChange(e)} value={obj1.region}>
+                    {/* <select name="region" onChange={(e) => handleInputChange(e)} value={obj1.region}>
                     {
                         regionData && 
                             regionData.map((item, idx) => 
                                 <option key={idx} value={item.name}>{item.name}</option>
                             )
                     }
-                    </select>
+                    </select> */}
+                    <TextField
+                        id="outlined-select-region"
+                        select
+                        name="region"
+                        value={obj1.region}
+                        onChange={(e) => handleInputChange(e)}
+                        size="small"
+                        >
+                        {regionData && 
+                            regionData.map((region, idx) => {
+                                return (
+                                    <MenuItem key={idx} value={region.name}>{region.name}</MenuItem>
+
+                                )}
+                            )}
+                    </TextField>
                 </div>
                 <div>
                     <label>Type</label>
-                    <button type="button" name="type" value="Active" onClick={(e) => handleInputChange(e)}>Active</button>
-                    <button type="button" name="type" value="New" onClick={(e) => handleInputChange(e)}>New</button>
-                    <button type="button" name="type" value="Dormant" onClick={(e) => handleInputChange(e)}>Dormant</button>
+                    <button type="button" className="type-btn" name="type" value="Active" onClick={(e) => handleInputChange(e)}>Active</button>
+                    <button type="button" className="type-btn" name="type" value="New" onClick={(e) => handleInputChange(e)}>New</button>
+                    <button type="button" className="type-btn" name="type" value="Dormant" onClick={(e) => handleInputChange(e)}>Dormant</button>
                 </div>
                 <button type="button" onClick={submitData}>Submit</button>
             </form>
